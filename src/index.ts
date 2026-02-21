81d4700575ba665c37675332f8ee3d5fca2b624c
Edvin CANDON <edvincandon@gmail.com>
2026-02-21 07:51:20 +0100

    feat: expose barrel file

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export { optimistron } from './optimistron';
+export { createTransition, createTransitions } from './actions';
+export {
+    selectConflictingTransition,
+    selectFailedTransition,
+    selectFailedTransitions,
+    selectIsConflicting,
+    selectIsFailed,
+    selectIsOptimistic,
+    selectOptimistic,
+} from './selectors';
+export { indexedStateFactory, type IndexedState } from './state/indexed';
+export { DedupeMode, Operation, OptimisticMergeResult, isTransition } from './transitions';
+
+export type { HandlerReducer } from './reducer';
+export type { BoundStateHandler, StateHandler, StateHandlerOptions, TransitionState } from './state';
+export type { TransitionAction, Transition } from './transitions';
