a6473b9de939aa459e0dd8df3217a2c63c7f80bb
Edvin CANDON <edvin@lostsolution.io>
2026-02-24 16:55:19 +0100

    chore: reflect action changes in usecases

--- usecases/lib/store/profile/actions.ts
+++ usecases/lib/store/profile/actions.ts
@@ -1,8 +1,8 @@
 import { createTransitions } from '~actions';
 import type { Profile } from '~usecases/lib/store/types';
 
-const updatePrepare = (item: Partial<Profile>) => ({ payload: { item }, transitionId: 'profile' });
-const clearPrepare = () => ({ payload: {}, transitionId: 'profile' });
+const updatePrepare = (item: Partial<Profile>) => ({ payload: item, transitionId: 'profile' });
+const clearPrepare = () => ({ payload: undefined as void, transitionId: 'profile' });
 
 export const updateProfile = createTransitions('profile::update')(updatePrepare);
 export const clearProfile = createTransitions('profile::clear')(clearPrepare);
